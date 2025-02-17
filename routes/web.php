8717b9871c659f8abead121737f504d2f0267784
sithumini Kaushalya <kaushalyasithumini29@gmail.com>
2025-02-17 23:05:02 +0530

    routes

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    $person = [
+        "name" => "Zura",
+        "email" => "zura@example.com",
+    ];
+    //dump($person); // laravel building function which build every information given variable
+    // dd($person); // "dd" - dump and die
     return view('welcome');
 });
 
-Route::get('/about', function() {
-    return view('about');
+// Route::get('/about', function() {
+//     return view('about');
+// });
+
+Route::view('/about', 'about'); // view function is used to return view
+
+// Route::get('/product/{id}', function(string $id) {
+//     return "Product id: $id";
+// });
+
+Route::get('/product/{category?}' , function(string $category = null) {
+    return "Product for category= $category";
 });
